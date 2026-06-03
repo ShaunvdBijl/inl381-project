e3c220af0cae5b46489c6a415d42ad2869fe3729
Shaun van der Bijl <shaunvdbijl@gmail.com>
2026-06-03 22:35:13 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const Subject = require('../models/Subject');
+const User = require('../Models/User');
+const Subject = require('../Models/Subject');
 const bcrypt = require('bcryptjs');
 
 // Login page
