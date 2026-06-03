374ca579cfd11e497e73fe841eeb7e065f9c428f
Shaun van der Bijl <shaunvdbijl@gmail.com>
2026-06-03 22:39:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 // Session middleware
 app.use(session({
