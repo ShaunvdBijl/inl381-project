4768871fc040acc11c9af23e1020712fa029d537
Shaun van der Bijl <shaunvdbijl@gmail.com>
2026-06-03 22:35:56 +0200

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const Subject = require('../models/Subject');
+const User = require('../Models/User');
+const Subject = require('../Models/Subject');
 const { requireAuth } = require('../middleware/auth');
 
 // Profile Settings - GET
